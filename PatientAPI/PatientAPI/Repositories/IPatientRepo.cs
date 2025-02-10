ed72572f3f2f753c199c33b646aa0621e20a90be
Parameswari Bala <parameswaribala@gmail.com>
2025-02-10 16:40:34 +0530

    updated

---
+++ PatientAPI/PatientAPI/Repositories/IPatientRepo.cs
@@ -0,0 +1,13 @@
+﻿using PatientAPI.Models;
+
+namespace PatientAPI.Repositories
+{
+    public interface IPatientRepo
+    {
+        Task<Patient> AddPatient(Patient Patient);
+
+        Task<bool> DeletePatient(long patientId);
+        Task<Patient> GetPatient(long patientId);
+        Task<IEnumerable<Patient>> GetAllPatients();
+    }
+}
---
+++ PatientAPI/PatientAPI/Repositories/PatientRepo.cs
@@ -0,0 +1,40 @@
+﻿using PatientAPI.Contexts;
+using PatientAPI.Models;
+
+namespace PatientAPI.Repositories
+{
+    public class PatientRepo : IPatientRepo
+    {
+        private PatientContext _dbContext;
+
+        public PatientRepo(PatientContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Patient> AddPatient(Patient Patient)
+        {
+            
+                var result = await this._dbContext.Patients.AddAsync(Patient);
+                await this._dbContext.SaveChangesAsync();
+                return result.Entity;
+
+            
+        }
+
+        public Task<bool> DeletePatient(long patientId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Patient>> GetAllPatients()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Patient> GetPatient(long patientId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
