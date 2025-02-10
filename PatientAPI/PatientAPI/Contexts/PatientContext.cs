5163a0bd2175eb049af986b7f251a0bc474420c5
Parameswari Bala <parameswaribala@gmail.com>
2025-02-10 16:10:46 +0530

    updated

---
+++ PatientAPI/PatientAPI/Contexts/PatientContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PatientAPI.Models;
+
+namespace PatientAPI.Contexts
+{
+    public class PatientContext:DbContext
+    {
+        public PatientContext(DbContextOptions<PatientContext> options) : base(options) { 
+        
+           Database.EnsureCreated();
+        }
+
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+
+
+    }
+}
