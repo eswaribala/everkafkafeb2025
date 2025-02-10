e0da3e754753b59eca0bfd1c8d00c53cb71ddd99
Parameswari Bala <parameswaribala@gmail.com>
2025-02-10 15:39:37 +0530

    updated

---
+++ PatientAPI/PatientAPI/Models/FullName.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PatientAPI.Models
+{
+    [Owned]
+    public class FullName
+    {
+        [Required]
+        [RegularExpression("^[a-zA-Z]{3,25}$",ErrorMessage ="First Name should be between 3 to 25 chars")]
+        [Column("First_Name", TypeName ="varchar(50)")]
+        public string FirstName { get; set; }
+       
+        [Column("Middle_Name", TypeName = "varchar(50)")]
+        public string MiddleName { get; set; }
+        [Required]
+        [RegularExpression("^[a-zA-Z]{3,25}$", ErrorMessage = "Last Name should be between 3 to 25 chars")]
+        [Column("Last_Name", TypeName ="varchar(50)")]
+        public string LastName { get; set; }
+    }
+}
---
+++ PatientAPI/PatientAPI/Models/Patient.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PatientAPI.Models
+{
+    public enum Gender { MALE,FEMALE,TRANSGENDER}
+    public class Patient
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("Patient_Id")]
+        public long PatientId { get; set; }
+
+
+        public FullName FullName { get; set; }
+        [Column("Gender")]
+        [EnumDataType(typeof(Gender))]
+        public Gender Gender { get; set; }
+      
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MM dd yyyy}")]
+        [Column("DOB")]
+        public DateTime DOB { get; set; }
+
+        [Column("Email")]
+        [RegularExpression("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$",
+            ErrorMessage = "Email Format Not matching")]
+        [DefaultValue("")]
+        public string Email { get; set; } = string.Empty;
+        [Column("Phone")]
+        [RegularExpression("^([+]\\d{2}[ ])?\\d{10}$",
+            ErrorMessage = "Mobile No Format Not matching")]
+        public long Phone { get; set; }
+
+    }
+}
